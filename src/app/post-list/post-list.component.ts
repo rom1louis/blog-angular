ad9a1947706c996ed7247b43ca2ea0d0fc64abb1
rom1louis <rom1louis@gmail.com>
2018-11-19 21:27:14 +0100

    mise a jour contenu + mise en forme

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -16,20 +16,20 @@ export class PostListComponent implements OnInit {
   
   posts = [
 	{
-	  title : 'titre 1 ',
-	  content : "contenu du titre 1 ",
+	  title : 'angularJS ',
+	  content : "AngularJS est un framework JavaScript libre et open source développé par Google. Il permet de développer des pages web. ",
 	  loveIts : 3,
 	  created_at : 'Feb 02, 2018'
 	},
 	{
-	  title : "titre 2 ",
-	  content : "contenu du titre 2 ",
+	  title : "Swift ",
+	  content : "Swift est un langage de programmation compilé multi-paradigmes développé par la société Apple. Il est destiné à la programmation d'applications sur les systèmes d'exploitation iOS, macOS, watchOS, tvOS ainsi que Linux. ",
 	  loveIts : -3,
 	  created_at : 'Jun 15, 2015'
 	},
 	{
-	  title : "titre 3 ",
-	  content : "contenu du titre 3 ",
+	  title : "langage C ",
+	  content : "C est un langage de programmation impératif généraliste. Inventé au début des années 1970 pour réécrire UNIX, C est devenu un des langages les plus utilisés ",
 	  loveIts : 0,
 	  created_at : this.dateTest
 	}
